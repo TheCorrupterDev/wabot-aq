ae160b044c663036ebaa57d019e4f3bc281e0976
TheCorrupterDev <73985463+TheCorrupterDev@users.noreply.github.com>
2021-09-16 20:04:20 +0200

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -3,7 +3,7 @@ handler.before = m => {
   let user = global.db.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
+Kamu berhenti AFK${user.afkReason ? ' dopo ' + user.afkReason : ''}
 Selama ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
@@ -17,8 +17,8 @@ Selama ${clockString(new Date - user.afk)}
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
     m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
+Non taggarlo!
+È AFK ${reason ? 'dengan alasan ' + ragione : 'tanpa alasan'}
 Selama ${clockString(new Date - afkTime)}
 `.trim())
   }
